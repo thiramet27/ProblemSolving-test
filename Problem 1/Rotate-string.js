9c4ceedc152d2b0997921241ba7b888f85b2e887
teera128 <156516395+teera128@users.noreply.github.com>
2025-01-29 00:45:23 +0700

    Delete Problem 1/Rotate-string.js

--- Problem 1/Rotate-string.js
+++
@@ -1,7 +0,0 @@
-var rotateString = function(s, goal) {
-    return s.length === goal.length && (s + s).includes(goal)
-};
-
-// Example usage:
-console.log(rotateString("abcde", "cdeab")); // Output: true
-console.log(rotateString("abcde", "abced")); // Output: false
